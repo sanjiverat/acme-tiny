40f99b22426e73f04919f48522a8fbac049d6da1
SE <sanjerat@gmail.com>
2016-10-01 04:23:04 -0400

    A small script to upload and install the generated SSL certificate on your cpanel account.

---
+++ save-cert.sh
@@ -0,0 +1,49 @@
+CPANEL_SERVER=cpanel_server.com
+CPANEL_USER=your_username
+CPANEL_PASSWORD=your_password
+
+function STDERR () {
+    cat - 1>&2
+}
+
+keyfile=none
+certfile=none
+
+while :; do
+  case "$1" in
+    "--help") 
+	  echo "save-cert.sh --key-file <domain.private.key> --cert-file <certificate.pem>" | STDERR
+	  exit 1 
+	  ;;
+    "--key-file")
+	  keyfile=$2
+	  shift
+	  ;;
+      "--cert-file")
+	  certfile=$2
+	  shift
+	  ;;
+      *)  
+	  echo "Unknown option:" $1 
+	  exit 1
+	  break
+  esac
+  shift
+done
+
+if [ $keyfile = "none" -o $certfile = "none" ]; then
+    echo "private key and certificate files are needed" | STDERR
+    echo "save-cert.sh --key-file <domain.private.key> --cert-file <certificate.pem>" | STDERR
+    exit 1
+fi
+
+
+URL="https://"$CPANEL_SERVER":2083/json-api/cpanel?cpanel_jsonapi_module=SSL&cpanel_jsonapi_func=installssl"
+
+CERT=`cat $certfile`
+CERT=$(python -c "import urllib; print urllib.quote('''$CERT''')")
+
+KEY=`cat $keyfile`
+KEY=$(python -c "import urllib; print urllib.quote('''$KEY''')")
+
+curl -u $CPANEL_USERNAME":"$CPANEL_PASSWORD -d 'crt='$CERT'&key='$KEY -X POST $URL
